8d5d0144809b913b37712f040fbe8ed40f76f912
Rakesh Singh <rs414057@gmail.com>
2017-03-16 15:12:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
-console.log('Loaded!');
+//console.log('Loaded!');
+var but = document.getElementById('count');
+var counter =0
+but.onclick=function(){
+  var myRequest = new XMLHttpRequest(); 
+    method = "GET",
+    url = "http://rs414057.imad.hasura-app.io/counter";
+    myRequest.open(method, url, true);
+    myRequest.onreadystatechange = function () {
+        if(myRequest.readyState === XMLHttpRequest.DONE && myRequest.status === 200) {
+            console.log(xhr.responseText);
+        }
+    };
+myRequest.send();
+};
